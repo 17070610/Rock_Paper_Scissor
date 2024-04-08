346b1da28726f94467902910f0391acc4aa47cb8
Schimea <schimea@hotmail.com>
2024-04-08 08:26:10 +0200

    Add script.js

--- rock_paper_scissor.js
+++
@@ -1,8 +0,0 @@
-const choices = ["Rock", "Paper", "Scissor"];
-function getComputerChoice(options){ 
-    randomChoice = Math.floor(Math.random() * choices.length)
-    return options[randomChoice]
-}
-
-const computerChoice = getComputerChoice(choices);
-console.log(computerChoice);
