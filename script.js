24295ed3b64cc7b765a1ec3fdd1d683a0eaabd73
17070610 <schimea@hotmail.com>
2024-04-10 10:57:07 +0200

    Add script.js

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
-const choices = ["rock", "paper", "scissor"];
-function getComputerChoice(options){ 
-    randomChoice = Math.floor(Math.random() * choices.length)
-    return options[randomChoice]
-}
-
-const computerChoice = getComputerChoice(choices);
-
-playerChoice = prompt("Rock, Paper or Scissor: ").toLowerCase();
-do{
-    if(choices.includes(playerChoice)){
-        console.log(playerChoice);
-        break;
-    }
-    else{
-        playerChoice = prompt("Please enter a valid Choice!! Rock, Paper or Scissor").toLowerCase();
-    }
-}while(true)
-
-function roundPlay(computerChoice, playerChoice) {
-  if(computerChoice === playerChoice){
-    return "It's a draw!!"
-  }else if(
-    (computerChoice === "rock" && playerChoice === "scissor") || 
-    (computerChoice === "scissor" && playerChoice === "paper") ||
-    (computerChoice === "paper" && playerChoice === "rock")
-    ){
-    return "Computer wins!!"
-  }else{
-    return "You win !!"
-  } 
-}
-
 function playGame(){
-    
+  const choices = ["rock", "paper", "scissor"];
+  function getComputerChoice(options){ 
+      randomChoice = Math.floor(Math.random() * choices.length)
+      return options[randomChoice]
+  }
+  
+  const computerChoice = getComputerChoice(choices);
+  
+  playerChoice = prompt("Rock, Paper or Scissor: ").toLowerCase();
+  do{
+      if(choices.includes(playerChoice)){
+          console.log(playerChoice);
+          break;
+      }
+      else{
+          playerChoice = prompt("Please enter a valid Choice!! Rock, Paper or Scissor").toLowerCase();
+      }
+  }while(true)
+  
+  function roundPlay(computerChoice, playerChoice) {
+    if(computerChoice === playerChoice){
+      return "It's a draw!!"
+    }else if(
+      (computerChoice === "rock" && playerChoice === "scissor") || 
+      (computerChoice === "scissor" && playerChoice === "paper") ||
+      (computerChoice === "paper" && playerChoice === "rock")
+      ){
+      return "Computer wins!!"
+    }else{
+      return "You win !!"
+    } 
+  }
+  console.log(roundPlay(computerChoice, playerChoice)) 
 }
+
+function playAgain(){
+  playGame()
+  let userChoice = prompt("Do you want to play again? (Yes/No):").toLowerCase();
+  do{
+    if(userChoice === "no"){
+      console.log("Thank you for Playing!!")
+      break;
+    } 
+    else if(userChoice === "yes"){
+      playGame();
+      break;
+    }  
+    else
+      prompt("Please enter a valid choice, (Yes/No)")
+  }while(true)
+}
+// playGame()
+playAgain()
